9552947843deee10739dc8a19165260cbd7912c5
Sergey <Sergey@Sergey-PC>
2013-04-22 18:12:01 +0400

    Перенос проекта13

--- src/java/test/Conv.java
+++ src/java/test/Conv.java
@@ -39,21 +39,31 @@ public class Conv {
     String s = "";
     int ID = 0;
     
-    String IdCoatuu_Obl = "";
+    
     
     String IdRegion1 = "";
     String IdRegion2 = "";
     String IdRegion3 = "";
     
+    
     String TypeRegion1 = "";
-    String NameRegion1 = "";    
+    String NameRegion1 = ""; 
+    String IdCoatuu1 = "";
 
     String TypeRegion2 = "";
     String NameRegion2 = "";
+    String IdCoatuu2 = "";
     
     String TypeRegion3 = "";
     String NameRegion3 = "";
+    String IdCoatuu3 = "";
     
+    String IdPolis4 = "";
+    String TypePolis4 = "";
+    String IdCoatuu4 = "";
+    
+    
+     
     String NamePolis = "";
     
   
@@ -66,16 +76,16 @@ public class Conv {
     
       
         
-        NamePolis = list3.get(i).toString();
-     
+      NamePolis = list3.get(i).toString(); // текущий полис
+      IdCoatuu4 = list1.get(i).toString();  
  
       
         //IdCoatuu = list1.get(i).toString();
         
        // sCurrentRow1 = list1.get(i).toString();
-        sCurrentRow2 = list2.get(i).toString();
-        sCurrentRow3 = list3.get(i).toString();
-        if ((list3.get(i).toString()).lastIndexOf("/") != -1) {
+      //  sCurrentRow2 = list2.get(i).toString();
+      //  sCurrentRow3 = list3.get(i).toString();
+        if ((list3.get(i).toString()).lastIndexOf("/") != -1) { // удаляем /М.СІМФЕРОПОЛЬ и.д
             String temp = (list3.get(i).toString());
             temp = (list3.get(i).toString()).substring(0, (list3.get(i).toString()).lastIndexOf("/"));
             list3.set(i, temp);
@@ -84,63 +94,93 @@ public class Conv {
         //System.out.println(lastIndexOf(sCurrentRow1,1)); 
         //int i = sCurrentRow1.lastIndexOf("/");
         
-
-        
-       
-        if ((list1.get(i).toString()).endsWith("00000000")) {
-            NameRegion1 = list3.get(i).toString();
-            IdCoatuu_Obl = list1.get(i).toString();
-            IdRegion1 = String.format("%07d", i + 1);
-        }
-        if ((list3.get(i).toString()).startsWith("АВТОНОМНА РЕСПУБЛІКА ")) {
-            TypeRegion1 = "1";
-            String temp = (list3.get(i).toString()).replace("АВТОНОМНА РЕСПУБЛІКА ","");
-            list1.set(i, temp);
-            
-        }
-        if (sCurrentRow3.contains("ОБЛАСТЬ")) { //endsWith
-            TypeRegion1 = "2";
-        }
-
-        
-       if (("00000".equals(sCurrentRow1.substring(5, 10))) &
-          (sCurrentRow1.substring(2, 3).equals("2")))        {
-        NameRegion2 = sCurrentRow3;
-        IdRegion2 = String.format("%07d", i+1);        
-       }
-       if (sCurrentRow3.contains(" РАЙОН")){
-           TypeRegion2 = "3"; 
-           }
       
        
-       
-          if (("8".equals(sCurrentRow1.substring(5, 6))) & 
-              (!"00".equals(sCurrentRow1.substring(6, 8))) &
-              ("00".equals(sCurrentRow1.substring(8, 10)))) {
-       NameRegion3 = sCurrentRow3;
-        IdRegion3 = String.format("%07d", i+1); 
-       ID = i+1;
-       }
-       if ("С".equals(sCurrentRow2)){
-           TypeRegion3 = "4"; 
-           }
-       if ("Щ".equals(sCurrentRow2)){
-           TypeRegion3 = "5"; 
-           }
-    
-       
-        if (("8".equals(sCurrentRow1.substring(5, 6))) & 
-              (!"00".equals(sCurrentRow1.substring(6, 8))) &
-              (!"00".equals(sCurrentRow1.substring(8, 10)))) {
+        if ((list1.get(i).toString()).endsWith("00000000")) { // если это ПЕРВАЯ ступень - область, АР
+            
            
-         //   IdRegion4 = String.format("%07d", i+1);
+            if ((list3.get(i).toString()).startsWith("АВТОНОМНА РЕСПУБЛІКА ")) {  //если фраза "АВТОНОМНА РЕСПУБЛІКА"
+            TypeRegion1 = "1";                                                // запоминаем тип
+            String sTemp = (list3.get(i).toString()).replace("АВТОНОМНА РЕСПУБЛІКА ",""); // удаляем
+            list3.set(i, sTemp);  //запоминаем
+        }
+        if ((list3.get(i).toString()).contains(" ОБЛАСТЬ")) { //endsWith
+            TypeRegion1 = "2";
+            String sTemp = (list3.get(i).toString()).replace(" ОБЛАСТЬ",""); // удаляем фразу " ОБЛАСТЬ"
+            list3.set(i, sTemp);  //запоминаем
+        }
             
-            sResultRow = "\n" + IdRegion1 +"   1   "+" "+NameRegion1+" "+TypeRegion1 + " " + IdCoatuu_Obl +    
-                     " | " + IdRegion2 +"   1   "+NameRegion2+" "+TypeRegion2 +
-                     " | " + IdRegion3 +"   1   "+NameRegion3+" "+TypeRegion3 +
-                     " | " + (i+1) + " " + IdRegion3 +"  "+NamePolis     ;  
+             
+            NameRegion1 = list3.get(i).toString();  // запоминаем название Области
+            IdCoatuu1 = list1.get(i).toString();  // запоминаем номер КОАТУ
+            IdRegion1 = String.format("%07d", i + 1);  // назначаем ИД
             
-            System.out.println(sResultRow);
+        }
+       
+
+        //========================================================
+        
+        if ( ((list1.get(i).toString()).substring(5, 10)).equals("00000") // // если это ВТОРАЯ ступень - район
+                & ((list1.get(i).toString()).substring(2, 3).equals("2"))) {
+            
+             if ((list3.get(i).toString()).contains(" РАЙОН")) {
+            TypeRegion2 = "3"; // тип Район
+            String sTemp = (list3.get(i).toString()).replace(" РАЙОН",""); // удаляем фразу " ОБЛАСТЬ"
+            list3.set(i, sTemp);  //запоминаем
+        }
+            
+            
+            NameRegion2 = (list3.get(i).toString());
+            IdRegion2 = String.format("%07d", i + 1);
+            IdCoatuu2 = list1.get(i).toString();  // запоминаем номер КОАТУ
+        }
+       
+      
+      //========================================================  
+       
+         if (("8".equals((list1.get(i).toString()).substring(5, 6))) // если это ТРЕТЬЯ ступень - сельрада
+                & (!"00".equals((list1.get(i).toString()).substring(6, 8)))
+                & ("00".equals((list1.get(i).toString()).substring(8, 10)))) {
+            NameRegion3 = list3.get(i).toString();
+            IdRegion3 = String.format("%07d", i + 1);
+            IdCoatuu3 = list1.get(i).toString();  // запоминаем номер КОАТУ
+           // ID = i + 1;           // связка с полисами
+            IdPolis4 = String.format("%07d", i+1); // связка с полисами
+        }
+         
+          TypeRegion3 = "7"; 
+//      
+    
+         //========================================================  
+        
+        if      (("8".equals((list1.get(i).toString()).substring(5, 6))) & // если это 4 ступень - село, селение
+                (!"00".equals((list1.get(i).toString()).substring(6, 8)))
+                & (!"00".equals((list1.get(i).toString()).substring(8, 10)))) {
+           
+                    if ("С".equals(list2.get(i).toString())) {
+                        TypePolis4 = "3";  // село
+                    }
+                    if ("Щ".equals(list2.get(i).toString())) {
+                        TypePolis4 = "4";   // селение
+                    }
+            
+                 
+             String IDs = String.format("%07d", i+1);
+           
+            
+         sResultRow = "\n" + IdRegion1 +"   1   "+" "+NameRegion1+" "+TypeRegion1 + " " + IdCoatuu1 +    
+                     " | " + IdRegion2 +"   1   "+NameRegion2+" "+TypeRegion2 + " " + IdCoatuu2 + 
+                     " | " + IdRegion3 +"   1   "+NameRegion3+" "+TypeRegion3 + " " + IdCoatuu3 + 
+                     " | " + (i+1) + " " + IdPolis4 +"  "+ TypePolis4 +"  "+  NamePolis + " " + IdCoatuu4    ;  
+            
+         List listTree1 = new ArrayList(42000); 
+         listTree1.add(IdRegion3);
+         List listTree2 = new ArrayList(42000); 
+         listTree2.add(IdRegion2);
+         List listTree3 = new ArrayList(42000); 
+         
+         
+            System.out.print(sResultRow);
             }
       // }
        //if (sResultRow == null) sResultRow = "s";
