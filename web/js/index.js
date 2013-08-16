56e5c2399c2c2335e62398ebb4ac2d9e3ec04fe0
Sergey <Sergey@Sergey-PC>
2013-08-16 17:42:49 +0400

    Перенос проекта 25 - значительная модификация интерфейса1

--- web/js/index.js
+++ web/js/index.js
@@ -358,10 +358,14 @@ var oData= { sDO: "theUserLogin",
  $.ajax({type:"POST",dataType:"json",url:"/Login",data:oData,async:/*false*/true
       ,success:function(o) {                                                                                 //    эта функция сработает гораздо позже, чем завершится выполнение всей функции doSend, т.к. это асинхронный режим работы.... потому безсмысленно обращаться за данными в конце ее(после: "dataFilter.... });") 
 
-           alert(o.sReturn);
-           if (o.sReturn == null)  
-                alert('в ответ: null');
-          if (o.sReturn == "Добро пожаловать на сайт!"){  (window.location.href="/index.jsp"); 
+           //alert(o.sReturn);
+          if (o.sReturn == null)  alert('в ответ вернулся : null');
+          
+          if (o.sReturn == "Добро пожаловать на сайт!"){  
+               
+               dhtmlx.message({ type:"default", expire:1000, text:"<br>  &nbsp; Добро пожаловать на сайт! <br><br>" });
+               setInterval(function() { (window.location.href="/index.jsp");  }, 1000);
+               
             }  
             else{
              
